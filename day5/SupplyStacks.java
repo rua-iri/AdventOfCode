a40fa489ab1fd50cb5100972fb25301db19c5f0a
rua-iri <rory.mcguigan@protonmail.com>
2022-12-11 22:52:11 +0000

    add solution for day 5 part one

---
+++ day5/SupplyStacks.java
@@ -0,0 +1,108 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+
+public class SupplyStacks {
+
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("./input.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            boolean readInstructions = false;
+
+            ArrayList<LinkedList<Character>> suppStacks = new ArrayList<>(9);
+            for(int x=0; x<9; x++) {
+                suppStacks.add(new LinkedList<Character>());
+            }
+            
+
+            while (line != null) {
+
+                if (readInstructions) {
+                    suppStacks.toString();
+                    processInstructions(line, suppStacks);
+                    
+                } else if(line.length()!=0 && line.charAt(1)!='1') {
+                    drawLayout(line, suppStacks);
+                }
+
+                if (line.length() == 0) {
+                    readInstructions = true;
+                }
+
+                line = br.readLine();
+            }
+
+            System.out.println("Part One Answer:");
+            for(int i=0; i<suppStacks.size(); i++) {
+                System.out.print(suppStacks.get(i).peek());
+            }
+            System.out.println();
+
+
+            br.close();
+            fr.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void processInstructions(String instructSet, ArrayList<LinkedList<Character>> queAra) {
+
+        int moveQuantity, moveFrom, moveTo;
+        char moveChar;
+
+
+        String[] instructAra = instructSet.split(" ");
+        moveQuantity = Integer.parseInt(instructAra[1]);
+        moveFrom = Integer.parseInt(instructAra[3]) - 1;
+        moveTo = Integer.parseInt(instructAra[5]) - 1;
+
+
+
+
+        // move boxes for a given ammount of times
+        for (int i = 0; i < moveQuantity; i++) {
+            moveChar = queAra.get(moveFrom).remove();
+            queAra.get(moveTo).addFirst(moveChar);
+        }
+
+
+    }
+
+    public static void drawLayout(String lne, ArrayList<LinkedList<Character>> queAra) {
+
+        int queueCount = 0;
+        LinkedList<Character> currentQue;
+        char boxContents;
+
+
+        //iterate through each element in the line and add to the respective queue
+        for(int i=1; i<lne.length(); i+=4) {
+            boxContents = lne.charAt(i);
+            currentQue = queAra.get(queueCount);
+
+            if(boxContents!=' ') {
+                currentQue.add(boxContents);
+
+                // remove old queue and add the new
+                queAra.remove(queueCount);
+                queAra.add(queueCount, currentQue);
+            }
+
+            queueCount++;
+        }
+
+    }
+
+
+}
