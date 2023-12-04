343683fdc7bb79685005218b1450b7993e2ac4b9
rua-iri <rory.mcguigan@protonmail.com>
2023-12-04 09:32:33 +0300

    complete: day one

--- 2023/day1/Main.java
+++ 2023/day1/Main.java
@@ -8,9 +8,11 @@ import java.util.HashMap;
 public class Main {
 
     public static final char[] numbersArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public static HashMap<String, String> numbersMap = new HashMap<String, String>();
 
     public static void main(String[] args) {
         int totalCalibrationValue = 0;
+        initialiseNumMap();
 
         File file = new File("input.txt");
         try {
@@ -19,7 +21,7 @@ public class Main {
             String ln = bufferedReader.readLine();
 
             while (ln != null) {
-                System.out.println(ln);
+                System.out.println("Complete Line: " + ln);
                 totalCalibrationValue += numbersParser(ln);
                 ln = bufferedReader.readLine();
             }
@@ -34,7 +36,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public static int numbersParser(String line) {
@@ -46,6 +47,9 @@ public class Main {
             if (checkNumberMatch(line.charAt(i))) {
                 stringBuilder.append(line.charAt(i));
                 break;
+            } else if (checkNumberMapMatch(line.substring(i), true)) {
+                stringBuilder.append(getNumberMapMatch(line.substring(i), true));
+                break;
             }
         }
 
@@ -53,23 +57,65 @@ public class Main {
         for (int i = line.length() - 1; i >= 0; i--) {
             if (checkNumberMatch(line.charAt(i))) {
                 stringBuilder.append(line.charAt(i));
+                System.out.println("End Substring: " + line.substring(0, i));
+                break;
+            } else if (checkNumberMapMatch(line.substring(0, i+1), false)) {
+                stringBuilder.append(getNumberMapMatch(line.substring(0, i+1), false));
+                System.out.println("End Substring: " + line.substring(0, i+1));
                 break;
             }
         }
 
         completeNumber = stringBuilder.toString();
-        System.out.println(completeNumber);
+        System.out.println("Complete Number: " + completeNumber + "\n");
         return Integer.parseInt(completeNumber);
     }
 
     public static boolean checkNumberMatch(char numChar) {
         for (char c : numbersArray) {
-            
-            if(numChar==c) {
+
+            if (numChar == c) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static boolean checkNumberMapMatch(String lineSubString, boolean isFirstNum) {
+
+        for (String s : numbersMap.keySet()) {
+            if (isFirstNum && lineSubString.startsWith(s)) {
+                return true;
+            } else if (!isFirstNum && lineSubString.endsWith(s)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static String getNumberMapMatch(String lineSubString, boolean isFirstNum) {
+        for (String s : numbersMap.keySet()) {
+            if (isFirstNum && lineSubString.startsWith(s)) {
+                return numbersMap.get(s);
+            } else if (!isFirstNum && lineSubString.endsWith(s)) {
+                return numbersMap.get(s);
+            }
+        }
+
+        return "";
+    }
+
+    public static void initialiseNumMap() {
+        numbersMap.put("one", "1");
+        numbersMap.put("two", "2");
+        numbersMap.put("three", "3");
+        numbersMap.put("four", "4");
+        numbersMap.put("five", "5");
+        numbersMap.put("six", "6");
+        numbersMap.put("seven", "7");
+        numbersMap.put("eight", "8");
+        numbersMap.put("nine", "9");
     }
 
 }
