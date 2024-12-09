9810b70903fb765332f3e6ec1401935fa80d8463
rua-iri <rory.mcguigan@protonmail.com>
2024-12-09 17:56:03 +0300

    add: solution for day 4 part 1

--- 2024/day4/main_1.py
+++ 2024/day4/main_1.py
@@ -1,6 +1,6 @@
 
 with open("input.txt") as file:
-    FILE_DATA: list = file.readlines()
+    FILE_DATA: list = file.read().strip().split("\n")
 SEARCH_HEIGHT: int = len(FILE_DATA)
 SEARCH_WIDTH: int = len(FILE_DATA[0])
 TARGET_WORD: str = "XMAS"
@@ -11,7 +11,7 @@ def generate_all_directions():
 
     for coord_x in range(-1, 2):
         for coord_y in range(-1, 2):
-            if coord_x != 0 and coord_y != 0:
+            if coord_x != 0 or coord_y != 0:
                 all_directions.append((coord_x, coord_y))
 
     return all_directions
@@ -20,15 +20,15 @@ def generate_all_directions():
 def check_letter_solutions(row_index, col_index, directions) -> int:
     direction_x, direction_y = directions
 
-    dx, dy = directions
-    for k, x in enumerate("XMAS"):
-        ii = row_index + k * dx
-        jj = col_index + k * dy
-        if not (0 <= ii < SEARCH_HEIGHT and 0 <= jj < SEARCH_WIDTH):
+    for xmas_index, xmas_letter in enumerate("XMAS"):
+        match_row_index = row_index + xmas_index * direction_x
+        match_col_index = col_index + xmas_index * direction_y
+        if not (0 <= match_row_index < SEARCH_HEIGHT and 0 <= match_col_index < SEARCH_WIDTH):
             return False
-        if FILE_DATA[ii][jj] != x:
+        if FILE_DATA[match_row_index][match_col_index] != xmas_letter:
             return False
-    return True
+
+    return 1
 
 
 def main():
