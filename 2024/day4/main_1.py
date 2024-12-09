f0ce74610cc006a8cea7fd3d04006d68da546bb9
rua-iri <rory.mcguigan@protonmail.com>
2024-12-09 17:37:01 +0300

    update: minor changes

--- 2024/day4/main_1.py
+++ 2024/day4/main_1.py
@@ -1,27 +1,48 @@
 
-TARGET_WORD: str = "xmas"
+with open("input.txt") as file:
+    FILE_DATA: list = file.readlines()
+SEARCH_HEIGHT: int = len(FILE_DATA)
+SEARCH_WIDTH: int = len(FILE_DATA[0])
+TARGET_WORD: str = "XMAS"
 
 
-def read_file_contents() -> list:
-    with open("input.txt") as file:
-        file_data = file.readlines()
+def generate_all_directions():
+    all_directions = []
 
-    return file_data
+    for coord_x in range(-1, 2):
+        for coord_y in range(-1, 2):
+            if coord_x != 0 and coord_y != 0:
+                all_directions.append((coord_x, coord_y))
+
+    return all_directions
 
 
-def check_letter_solutions() -> int:
-    pass
+def check_letter_solutions(row_index, col_index, directions) -> int:
+    direction_x, direction_y = directions
+
+    dx, dy = directions
+    for k, x in enumerate("XMAS"):
+        ii = row_index + k * dx
+        jj = col_index + k * dy
+        if not (0 <= ii < SEARCH_HEIGHT and 0 <= jj < SEARCH_WIDTH):
+            return False
+        if FILE_DATA[ii][jj] != x:
+            return False
+    return True
 
 
 def main():
-    file_data: list = read_file_contents()
 
-    search_height: int = len(file_data)
-    search_width: int = len(file_data[0])
+    total_count: int = 0
+    all_directions: list = generate_all_directions()
 
-    for y in range(search_height):
-        for x in range(search_width):
-            pass
+    for row_index in range(SEARCH_HEIGHT):
+        for col_index in range(SEARCH_WIDTH):
+            for direction in all_directions:
+                total_count += check_letter_solutions(
+                    row_index, col_index, direction)
+
+            print(total_count)
 
 
 if __name__ == "__main__":
