b04f8d243803960e29ad173788134a54a6010a0e
rua-iri <rory.mcguigan@protonmail.com>
2024-12-10 09:34:28 +0300

    update: ensure type consistency

--- 2024/day4 (Python)/main_1.py
+++ 2024/day4 (Python)/main_1.py
@@ -1,4 +1,3 @@
-
 with open("input.txt") as file:
     FILE_DATA: list = file.read().strip().split("\n")
 SEARCH_HEIGHT: int = len(FILE_DATA)
@@ -23,24 +22,24 @@ def check_letter_solutions(row_index, col_index, directions) -> int:
     for xmas_index, xmas_letter in enumerate("XMAS"):
         match_row_index = row_index + xmas_index * direction_x
         match_col_index = col_index + xmas_index * direction_y
-        if not (0 <= match_row_index < SEARCH_HEIGHT and 0 <= match_col_index < SEARCH_WIDTH):
-            return False
+        if not (
+            0 <= match_row_index < SEARCH_HEIGHT and 0 <= match_col_index < SEARCH_WIDTH
+        ):
+            return 0
         if FILE_DATA[match_row_index][match_col_index] != xmas_letter:
-            return False
+            return 0
 
     return 1
 
 
 def main():
-
     total_count: int = 0
     all_directions: list = generate_all_directions()
 
     for row_index in range(SEARCH_HEIGHT):
         for col_index in range(SEARCH_WIDTH):
             for direction in all_directions:
-                total_count += check_letter_solutions(
-                    row_index, col_index, direction)
+                total_count += check_letter_solutions(row_index, col_index, direction)
 
             print(total_count)
 
